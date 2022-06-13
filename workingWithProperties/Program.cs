ae8204213f89eaa75b77421844ae58bb769ecf0d
Михаил Денисов <migelius.invesy@gmail.com>
2022-06-13 18:22:57 +0300

    Re

--- workingWithProperties/Program.cs
+++ workingWithProperties/Program.cs
@@ -3,9 +3,9 @@
     static void Main(string[] args)
     {
         Renderer renderer = new Renderer();
-        Player player = new Player(4, 15);
+        Player player = new Player(4, 6, 'X');
 
-        renderer.DrawPlayer(player.PositionX, player.PositionY);
+        renderer.DrawPlayer(player.PositionX, player.PositionY, player.Symbol);
     }
 }
 
@@ -20,12 +20,15 @@ class Renderer
 
 class Player
 {
+    public char Symbol { get; private set; }
     public int PositionX { get; private set; }
     public int PositionY { get; private set; }
 
-    public Player(int positionX, int positionY)
+    public Player(int positionX, int positionY, char symbol)
     {
+
         PositionX = positionX;
         PositionY = positionY;
+        Symbol = symbol;
     }
 }
